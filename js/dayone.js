9ea4edc6f0cf5a4d19d4ba7f6212184ed57f07ed
sleamer <sleamer@hotmail.com>
2014-09-18 14:51:16 -0700

    Added tooltips to worldmap,added imageresizer, added qtip, and changed styling.

--- js/dayone.js
+++ js/dayone.js
@@ -1,8 +1,18 @@
-	$(document).ready(function(){
-			$('#dayoneCarousel').slick({
-			  fade: true,
-			  speed: 500,
-			  autoplay: true,
-			  arrows: true,
-			});	
+$(document).ready(function(){
+	$('#dayoneCarousel').slick({
+	  fade: true,
+	  speed: 500,
+	  autoplay: true,
+	  arrows: true,
+	});	
+	$("area").each(function(index, element) {
+		var link = $(this);
+		$(link).qtip({
+			content: link.attr("title"),
+			style: {
+             	width: 100,
+             	classes: 'qtip-dayone qtip-rounded'
+           },
+		});
 	});
+});
