f440dc8fee6d6102fd9fc98bb3ace364c62a1c51
sleamer <sleamer@hotmail.com>
2014-10-12 16:38:48 -0700

    Redesign of the waterbag.html page, added slider and many new css classes.

--- js/dayone.js
+++ js/dayone.js
@@ -15,4 +15,20 @@ $(document).ready(function(){
            },
 		});
 	});
+	$('#waterbagCarousel').slick({
+	  fade: true,
+	  speed: 500,
+	  autoplay: true,
+	  arrows: true,
+	});	
+	$("area").each(function(index, element) {
+		var link = $(this);
+		$(link).qtip({
+			content: link.attr("title"),
+			style: {
+             	width: 160,
+             	classes: 'qtip-dayone qtip-rounded'
+           },
+		});
+	});
 });
