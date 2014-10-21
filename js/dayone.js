0711a82cdcc6b2c6a33e3658b4e989d9a7573bd7
sleamer <sleamer@hotmail.com>
2014-10-21 12:57:06 -0700

    New version of Community page with embedded videos

--- js/dayone.js
+++ js/dayone.js
@@ -21,14 +21,4 @@ $(document).ready(function(){
 	  autoplay: true,
 	  arrows: true,
 	});	
-	$("area").each(function(index, element) {
-		var link = $(this);
-		$(link).qtip({
-			content: link.attr("title"),
-			style: {
-             	width: 160,
-             	classes: 'qtip-dayone qtip-rounded'
-           },
-		});
-	});
 });
